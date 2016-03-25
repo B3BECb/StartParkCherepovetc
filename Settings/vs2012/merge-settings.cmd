92ff1281f51a9beb2ec375db75dd8d81715d577a
daniil <danith@yandex.ru>
2016-03-25 22:49:00 +0300

    Cleaned repository

--- Settings/vs2012/merge-settings.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-..\tools\merge-settings.exe -w settings.whitelist -u "%USERPROFILE%\Documents\Visual Studio 2012\Settings\CurrentSettings.vssettings" -t "editor.vssettings" -o merged.vssettings
