85393c5b9276f920146703ae2baf7130ffe06f97
@ixlz <xlz.lux@gmail.com>
2016-04-19 14:09:01 +0300

    Potoki_group №6

---
+++ src/potoki_6/potoki_6/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace potoki_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._prbProces = new System.Windows.Forms.ProgressBar();
+            this._btnreset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _prbProces
+            // 
+            this._prbProces.Location = new System.Drawing.Point(12, 12);
+            this._prbProces.Name = "_prbProces";
+            this._prbProces.Size = new System.Drawing.Size(268, 23);
+            this._prbProces.TabIndex = 0;
+            // 
+            // _btnreset
+            // 
+            this._btnreset.Location = new System.Drawing.Point(12, 41);
+            this._btnreset.Name = "_btnreset";
+            this._btnreset.Size = new System.Drawing.Size(268, 23);
+            this._btnreset.TabIndex = 1;
+            this._btnreset.Text = "Start";
+            this._btnreset.UseVisualStyleBackColor = true;
+            this._btnreset.Click += new System.EventHandler(this._btnreset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(292, 78);
+            this.Controls.Add(this._btnreset);
+            this.Controls.Add(this._prbProces);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Поток";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar _prbProces;
+        private System.Windows.Forms.Button _btnreset;
+    }
+}
+
---
+++ src/potoki_6/potoki_6/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace potoki_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void _btnreset_Click(object sender, EventArgs e)
+        {
+            int Count = 10000;
+            Random Rnd = new Random();
+            int[] Mas_1 = new int[Count];
+            int[] Mas_2 = new int[Count];
+            for (int i = 0; i < Count; ++i)
+            {
+                Mas_1[i] = Rnd.Next();
+                Mas_2[i] = Rnd.Next();
+            }
+            _prbProces.Maximum = Count;
+            _prbProces.Value = 0;
+
+            Thread t = new Thread(new ThreadStart(delegate {
+                for (int i = 0; i < Count; ++i)
+                {
+                    this.Invoke(new ThreadStart(delegate
+                    {
+
+                        _prbProces.Value++;
+                    }));
+                }
+            }));
+            t.Start();
+        }
+    }
+}
